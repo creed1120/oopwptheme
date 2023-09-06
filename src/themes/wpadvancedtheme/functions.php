300108190f46694f5a0ea7efc03d05a860a807f0
creed1120 <cedricareed@gmail.com>
2023-09-05 21:28:46 -0700

    added babel and webpack config

--- src/themes/wpadvancedtheme/functions.php
+++ src/themes/wpadvancedtheme/functions.php
@@ -17,6 +17,24 @@ if ( ! defined( 'WPADVANCEDTHEME_DIR_PATH' ) ) {
 if ( ! defined( 'WPADVANCEDTHEME_DIR_URI' ) ) {
    define( 'WPADVANCEDTHEME_DIR_URI', untrailingslashit( get_template_directory_uri() ) );
 }
+if ( ! defined( 'WPADVANCEDTHEME_BUILD_URI' ) ) {
+   define( 'WPADVANCEDTHEME_BUILD_URI', untrailingslashit( get_template_directory_uri() ) . '/assets/build' );
+}
+if ( ! defined( 'WPADVANCEDTHEME_BUILD_JS_URI' ) ) {
+   define( 'WPADVANCEDTHEME_BUILD_JS_URI', untrailingslashit( get_template_directory_uri() ) . '/assets/build/js' );
+}
+if ( ! defined( 'WPADVANCEDTHEME_BUILD_JS_DIR_PATH' ) ) {
+   define( 'WPADVANCEDTHEME_BUILD_JS_DIR_PATH', untrailingslashit( get_template_directory() ) . '/assets/build/js' );
+}
+if ( ! defined( 'WPADVANCEDTHEME_BUILD_IMG_URI' ) ) {
+   define( 'WPADVANCEDTHEME_BUILD_IMG_URI', untrailingslashit( get_template_directory_uri() ) . '/assets/build/src/img' );
+}
+if ( ! defined( 'WPADVANCEDTHEME_BUILD_CSS_URI' ) ) {
+   define( 'WPADVANCEDTHEME_BUILD_CSS_URI', untrailingslashit( get_template_directory_uri() ) . '/assets/build/src/css' );
+}
+if ( ! defined( 'WPADVANCEDTHEME_BUILD_CSS_DIR_PATH' ) ) {
+   define( 'WPADVANCEDTHEME_BUILD_CSS_DIR_PATH', untrailingslashit( get_template_directory() ) . '/assets/build/src/css' );
+}
 
 require_once WPADVANCEDTHEME_DIR_PATH . '/includes/helpers/autoloader.php';
 require_once WPADVANCEDTHEME_DIR_PATH . '/includes/helpers/template-tags.php';
--- src/themes/wpadvancedtheme/includes/classes/class-assets.php
+++ src/themes/wpadvancedtheme/includes/classes/class-assets.php
@@ -32,16 +32,19 @@ class Assets {
         wp_register_style( 'style-css', get_stylesheet_uri(), false, filemtime( WPADVANCEDTHEME_DIR_PATH . '/style.css' ), 'all' );
         wp_register_style( 'bootstrap-css', WPADVANCEDTHEME_DIR_URI . '/assets/src/lib/css/bootstrap.min.css', [], false, 'all' );
         wp_register_style( 'custom', WPADVANCEDTHEME_DIR_URI . '/assets/css/custom.css', [], false, 'all' );
+        wp_register_style( 'fonts-css', WPADVANCEDTHEME_DIR_URI . '/assets/src/lib/fonts/fonts.css', [], false, 'all' );
 
         // Enqueue the specific Stylesheet by the handle
         wp_enqueue_style('style-css');
         wp_enqueue_style('bootstrap-css');
         wp_enqueue_style('custom');
+        wp_enqueue_style('fonts-css');
     }
 
     // ENQUEUE SCRIPTS
     public function register_scripts() {
-        wp_register_script( 'main-js', WPADVANCEDTHEME_DIR_URI . '/assets/src/js/main.js', ['jquery'], filemtime( WPADVANCEDTHEME_DIR_PATH . '/assets/src/js/main.js' ), true);
+        // added WP get_template_directory_uri() because it grabs the Docker path
+        wp_register_script( 'main-js', get_template_directory_uri() . '/assets/build/js/main.js', ['jquery'], filemtime( WPADVANCEDTHEME_BUILD_JS_DIR_PATH . '/main.js' ), true);
         wp_register_script( 'bootstrap-js', WPADVANCEDTHEME_DIR_URI . '/assets/src/lib/js/bootstrap.min.js', ['jquery'], false, true);
 
         // Enqueue the specific Stylesheet or Script by the handle
